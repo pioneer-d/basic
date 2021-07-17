a3db0a0fe6d7ac0aff36ac2c1de2e9eb04209c62
pioneer-d <pioneer-d@naver.com>
2021-07-17 17:22:13 +0900

    Java study day3
    클래스, 필드, 생성자, getter,setter

---
+++ StudyJava/study1/src/day3/Class4.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class Class4 {	//按眉 积己 棺 积己磊 劝侩
+	
+	public static void main(String[] args) {
+		
+		Class3 c = new Class3();
+		Class3 c3 = new Class3(24, "JUN", 7);
+		
+		System.out.println(c3.getAge());
+		System.out.println(c3.getName());
+		System.out.println(c3.getNum());
+		
+	}
+
+}
