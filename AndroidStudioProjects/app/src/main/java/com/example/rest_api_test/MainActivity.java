7151ce5aa5b8d89a147310f19ace8b50965c95fa
pioneer-d <pioneer-d@naver.com>
2022-04-18 19:16:59 +0900

    Snaptag Android API_TEST_1

--- AndroidStudioProjects/app/src/main/java/com/example/rest_api_test/MainActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/example/rest_api_test/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
     String receiveJson = "";
 
+    private JSONArray jsonArray;
+    private JSONObject jsonObject;
+
+    private Button button;
     private TextView textview;
 
     //Project 초기화
@@ -29,25 +39,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (Button) findViewById(R.id.button);
         textview = (TextView) findViewById(R.id.textview);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readJSON();
+            }
+        });
+
+        // -> URL 다시 찾아보고 받아노느 것 부터 ㄱㄱ
+
+    }
+    private void readJSON() {
+        //Json 데이터 URL로 받아오기
         try {
             is = new URL(url).openStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, "UTF-8"));
             String str;
             StringBuffer buffer = new StringBuffer();
-            while((str = rd.readLine()) != null){
+            while ((str = rd.readLine()) != null) {
                 buffer.append(str);
             }
             receiveJson = buffer.toString();
         } catch (IOException e) {
             e.printStackTrace();
+            Log.d(activityName, "JSON URL로 받기 실패");
         }
 
-        Log.d(activityName,"getStringBuffer : "+receiveJson);
-        textview.setText(receiveJson);
+        //JSON 파싱
+        try {
+            JSONObject jsonObject1;
+            jsonArray = new JSONArray(receiveJson);
+            String id;
+            String title;
+            String name;
+            for (int i = 0; i < jsonArray.length(); i++) {
+                jsonObject = jsonArray.getJSONObject(i);
+                jsonObject1 = jsonObject.getJSONObject("posts");
+                id = jsonObject1.getString("id");
+                title = jsonObject1.getString("title");
+                name = jsonObject.getString("name");
+                textview.setText("id : "+ id + "title : " + title + "name : " + name);
+                Log.d(activityName,"id : "+ id + "title : " + title + "name : " + name);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
-
 
 
 
