1977324fe5c1899c604a5f112d9bbcf6334abe35
pioneer-d <pioneer-d@naver.com>
2022-05-11 17:48:48 +0900

    Snaptag Android_LabCode_TEST_19(Success get Location Data)

--- AndroidStudioProjects/BEIDOU_Test_Project2/app/src/main/java/com/snaptag/beidou_test_project2/MainActivity.java
+++ AndroidStudioProjects/BEIDOU_Test_Project2/app/src/main/java/com/snaptag/beidou_test_project2/MainActivity.java
@@ -18,71 +18,90 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    static String thisName = "MainActivity";
+
     LocationManager locationManager;
     Button button;
     TextView text;
+    Location location;
+    ToggleButton tb;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(thisName,"onCreate() 실행");
         setContentView(R.layout.activity_main);
-        Log.d("this Version :",String.valueOf(Build.VERSION.SDK_INT));  //-> 샤오미 : 30
+        Log.d(thisName,"this Version :" +String.valueOf(Build.VERSION.SDK_INT));  //-> 샤오미 : 30
 
         button = (Button) findViewById(R.id.button);
-        text = (TextView)findViewById(R.id.loadGps);
+        text = (TextView) findViewById(R.id.loadGps);
+        tb = (ToggleButton) findViewById(R.id.toggle1);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Log.d(thisName,"locationManager : "+ String.valueOf(locationManager));
 
-        button.setOnClickListener(new View.OnClickListener() {
+//        if ( Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+//            ActivityCompat.requestPermissions( MainActivity.this, new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION }, 0 );
+//        } else {
+//            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//            if(location == null) {
+//                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+//            }
+//        }
+
+        tb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if ( Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-                    ActivityCompat.requestPermissions( MainActivity.this, new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION }, 0 );
-                } else {
-                    //Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    //Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                    //Location location = locationManager.getLastKnownLocation(LocationManager.KEY_LOCATIONS);
-                    //Location location = locationManager.getLastKnownLocation(LocationManager.FUSED_PROVIDER);
-                    //Location location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-                    List<String> providers = locationManager.getProviders(false);
-                    Location location = null;
-                    Log.d("providers.size() : ",String.valueOf(providers.size()));
-
-                    for (int i=0; i<3; i++) {
-                        location = locationManager.getLastKnownLocation(providers.get(i));
-                        Log.d("qweqwe location : ",String.valueOf(providers.get(i)));
-                        if (location != null) break;
+                try {
+                    if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                    } else {
+                        if (tb.isChecked()) {
+                            text.setText("수신중..");
+                            // GPS 제공자의 정보가 바뀌면 콜백하도록 리스너 등록하기~!!!
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                                    1, // 통지사이의 최소 변경거리 (m)
+                                    gpsLocationListener);
+                            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                                    1, // 통지사이의 최소 변경거리 (m)
+                                    gpsLocationListener);
+                            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, // 등록할 위치제공자
+                                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                                    1, // 통지사이의 최소 변경거리 (m)
+                                    gpsLocationListener);
+                        } else {
+                            text.setText("위치정보 미수신중");
+                            locationManager.removeUpdates(gpsLocationListener);  //  미수신할때는 반드시 자원해체를 해주어야 한다.
+                        }
                     }
-
-                    String provider = location.getProvider();
-                    double longitude = location.getLongitude();
-                    double latitude = location.getLatitude();
-                    double altitude = location.getAltitude();
-                    text.setText("위치정보 : " + provider + "\n" + "위도 : " + longitude + "\n" + "경도 : " + latitude + "\n" + "고도 : " + altitude);
-
-                    //위치정보를 원하는 시간, 거리마다 갱신해줌.
-//                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, gpsLocationListener);
-//                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, gpsLocationListener);
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                    e.getMessage();
                 }
             }
+
 
         });
 
     }
 
-
     final LocationListener gpsLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
+            Log.d(thisName,"onLocationChanged(  ) 실행");
             String provider = location.getProvider();
             double longitude = location.getLongitude();
             double latitude = location.getLatitude();
             double altitude = location.getAltitude();
-            text.setText("위치정보 : " + provider + "\n" + "위도 : " + longitude + "\n" + "경도 : " + latitude + "\n" + "고도 : " + altitude);
+            double accuracy = location.getAccuracy();
+            text.setText("위치정보 : " + provider + "\n" + "위도 : " + longitude + "\n" + "경도 : " + latitude + "\n" + "고도 : " + altitude + "\n" + "정확도 : " +accuracy);
         }
 
         public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -94,7 +113,4 @@ public class MainActivity extends AppCompatActivity {
         public void onProviderDisabled(String provider) {
         }
     };
-
-
-
 }
