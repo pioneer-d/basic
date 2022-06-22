fb5ceb0615c813a936f625df6ed9194c6d0cfd58
pioneer-d <pioneer-d@naver.com>
2022-06-22 18:50:43 +0900

    Study_Kotlin_1
    변수, nullable, new 생성자

---
+++ AndroidStudioProjects/kotlin_project/app/src/main/java/com/basic/kotlin_project/day_1/Person.java
@@ -0,0 +1,11 @@
+package com.basic.kotlin_project.day_1;
+
+public class Person {
+
+    String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+}
---
+++ AndroidStudioProjects/kotlin_project/app/src/main/java/com/basic/kotlin_project/day_1/ja_variable_1.java
@@ -0,0 +1,28 @@
+package com.basic.kotlin_project.day_1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ja_variable_1 {
+
+    public static void main(String[] args) {
+
+        long number1 = 10L;
+        final long number2 = 10L;
+        // -> 이 둘의 차이는 가변인가 불변인가 차이.
+
+
+        long number3 = 10L;
+        Long nubmer4 = 1_000L;
+        // -> 이 둘의 차이는 primitive type인가, reference type인가.
+
+
+        Person person = new Person("동준영");
+        //자바는 객체를 생성하기 위해 new 키워드를 사용한다.
+
+
+    }
+
+
+
+}
